3cd0b1e4f9eca8ff9c3a25a20a8fc198a755cdf3
Aydan Olaez <aolaez@ucsc.edu>
2025-11-01 08:42:37 +0000

    started processes, cont work off of vm

---
+++ lib/cpu_proc.c
@@ -0,0 +1,40 @@
+#include <cpu.h>
+#include <emu.h>
+
+// Processes CPU Instructions
+static void proc_none(cpu_context *ctx) {
+    printf("INVALID INSTRUCTION!\n");
+    exit(-7);
+}
+
+static void proc_none(cpu_context *ctx) {
+    // TODO;
+}
+
+static bool check_cond(cpu_context *ctx) {
+    bool z = CPU_FLAG_Z;
+    bool c = CPU_FLAG_C;
+
+    switch(ctx->cur_inst->cond) {
+        case CT_NONE: return true;
+        case CT_C: return c;
+        case CT_NC: return !c;
+        case CT_Z: return z;
+        case CT_NZ: return !z;
+    }
+
+    return false;
+}
+
+static void proc_jp(cpu_context *ctx) {
+    if (check_cond(ctx)){
+        ctx->regs.pc = ctx->fetch_data;
+        emu_cycles(1);
+    }
+}
+
+static IN_PROC processors[] = {
+    [IN_NONE] = proc_none, // IN_NONE
+    [IN_LD] = proc_ld,
+    [IN_JP] = proc_jp
+};
